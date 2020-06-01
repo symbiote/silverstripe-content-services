af93fb874eb026f65c5bbb716715b3e6ff5d8d3a
jcarter <josh@symbiote.com.au>
2020-06-01 13:01:33 +1000

    chore(7.4): Remove depreciated syntax for PHP 7.4

--- code/content/FileContentReader.php
+++ code/content/FileContentReader.php
@@ -1,62 +1,62 @@
 <?php
 
 /**
- * Content reader to read content from the filesystem 
- * 
+ * Content reader to read content from the filesystem
+ *
  * @author marcus@symbiote.com.au
  * @license BSD License http://silverstripe.org/bsd-license/
  */
 class FileContentReader extends ContentReader {
 
-	/** 
-	 * Where should file assets be written to initially? 
-	 * 
+	/**
+	 * Where should file assets be written to initially?
+	 *
 	 * @var string
 	 */
 	public $basePath = 'content';
-	
+
 	/**
 	 * The base URL to prefix things with
 	 *
 	 * @var string
 	 */
 	public $baseUrl = null;
-	
+
 	public function getURL() {
-		
+
 		return $this->getBaseUrl() . '/' /*. $path . '/' */ . $this->getId();
 	}
-	
+
 	public function getBaseUrl() {
 		// if none configured, use a URL created from the SS base url
 		if (!$this->baseUrl) {
 			$path = $this->basePath;
-		
-			if ($path{0} == '/') {
+
+			if ($path[0] == '/') {
 				$path = str_replace(Director::baseFolder(), '', $path);
 			}
-			
+
 			$this->baseUrl = Director::absoluteBaseURL() . $path;
 		}
 		return rtrim($this->baseUrl, '/');
 	}
-	
+
 	public function isReadable() {
 		$path = $this->getPath($this->getId());
 		return is_readable($path);
 	}
-	
+
 		/**
 	 * An S3 object is listable if its content type is a directory
-	 * 
+	 *
 	 * @return boolean
 	 */
 	public function isListable() {
 		$path = $this->getPath($this->getId());
-		
+
 		return is_dir($path);
 	}
-	
+
 	public function getList() {
 		$list = array();
 
@@ -72,14 +72,14 @@ class FileContentReader extends ContentReader {
 
 		return $list;
 	}
-	
+
 	/**
 	 * Read content back to the user
 	 *
 	 * @return string
 	 */
 	public function read() {
-		
+
 		$id = $this->getId();
 		$path = $this->getPath($id);
 		if (!is_readable($path)) {
@@ -88,13 +88,13 @@ class FileContentReader extends ContentReader {
 
 		return file_get_contents($path);
 	}
-	
+
 	protected function getPath($id) {
 		$base = $this->basePath;
-		if ($id{0} == '/') {
+		if ($id[0] == '/') {
 			$path = $id;
 		} else {
-			if ($base{0} == '/') {
+			if ($base[0] == '/') {
 				$path = $base . '/' . $id;
 			} else {
 				$path = Director::baseFolder() . '/' . $base . '/' . $id;
